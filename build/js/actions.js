b00137fa7b1f84205cf62f138b8f1dd6f12ffa0f
TRenfro <v.renfro6@gmail.com>
2017-10-17 16:50:02 -0500

    toggles fixed bottom location menu on scroll

--- build/js/actions.js
+++ build/js/actions.js
@@ -1,7 +1,7 @@
 // main
-$( document ).ready(function() {
+$(document).ready(function () {
 
-    $(".action-icon").click(function(){
+    $(".action-icon").click(function () {
         toggleLocationsContainer();
     })
 
@@ -14,19 +14,50 @@ $( document ).ready(function() {
 // Helpers
 function toggleLocationsContainer() {
 
-  var activated = $(".action-icon").hasClass("fa-plus");
+    var activated = $(".action-icon").hasClass("fa-plus");
 
-  $(".locations-container").toggleClass("hide-locations-container");
+    $(".locations-container").toggleClass("hide-locations-container");
 
-  if(activated) {
+    if (activated) {
 
-      $(".action-icon").removeClass("fa-plus");
-      $(".action-icon").addClass("fa-minus");
+        $(".action-icon").removeClass("fa-plus");
+        $(".action-icon").addClass("fa-minus");
 
-  } else {
+    } else {
 
-     $(".action-icon").removeClass("fa-minus");
-     $(".action-icon").addClass("fa-plus");
+        $(".action-icon").removeClass("fa-minus");
+        $(".action-icon").addClass("fa-plus");
 
-  }
+    }
 }
+
+
+//Sticky Locatoins Menu on Scroll
+// $(function () {
+//     var menu = $('.locations-footer-menu');
+//     var hieghtThreshold = $("#footer").offset().top;
+//     $(window).scroll(function () {
+//         var scroll = $(window).scrollTop();
+
+//         if (scroll >= hieghtThreshold) {
+//             menu.addClass('sticky');
+//         } else {
+//             menu.removeClass('sticky');
+//         }
+//     });
+// })
+
+$(function() {
+    var menu = $('.locations-footer-menu');
+    var hieghtThreshold = $("footer").offset().top-500;
+    var hieghtThreshold_end  = $("footer").offset().top +$("footer").height() ;
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop();
+
+        if (scroll >= hieghtThreshold && scroll <=  hieghtThreshold_end ) {
+            menu.removeClass('sticky');
+        } else {
+            menu.addClass('sticky');            
+        }
+    });
+})
